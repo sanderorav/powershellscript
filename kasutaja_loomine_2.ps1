1ac1206eef1cad361cd6d42750437786e2ea4f98
Sander Orav <sander.orav@voco.ee>
2026-03-02 07:09:00 +0000

    Kasutaja loomine csv failist

---
+++ kasutaja_loomine_2.ps1
@@ -0,0 +1,15 @@
+﻿# Fail, kus on kasutajate andmed
+$fail = "C:\Users\Sander\powershellscript\kasutajad.csv"
+
+# Loeme faili sisu
+$kasutajad = Import-Csv $fail -Encoding Default -Delimiter ";"
+
+# Salvestame iga rea $kasutaja muutujasse
+foreach ($kasutaja in $kasutajad) {
+$kasutajanimi = $kasutaja.kasutajanimi
+$taisnimi = $kasutaja.taisnimi
+$kontokirjeldus = $kasutaja.kontokirjeldus
+$parool = $kasutaja.parool | ConvertTo-SecureString -AsPlainText -Force
+# Teeme antud teabe põhjal kasutaja
+New-LocalUser -Name $kasutajanimi -Password $parool -FullName "$taisnimi" -Description "$kasutajakirjeldus"
+}
