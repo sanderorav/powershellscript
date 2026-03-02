b7ddfc044007310f99d824a3fa47e6e126a86f9d
Sander Orav <sander.orav@voco.ee>
2026-03-02 07:02:18 +0000

    Kommenteeritud

--- kasutaja_loomine_1.ps1
+++ kasutaja_loomine_1.ps1
@@ -1,9 +1,11 @@
 ﻿if($args.Count -ne 3){
+# Teavitame kasutajat, kui sisestatakse vale arv argumente
 echo 'Viga! Skripti käivitamiseks sisesta: .\skriptinimi kasutajanimi "Eesnimi Perenimi" "Konto kirjeldus"'
 } else {
 $kasutajanimi = $args[0]
 $taisnimi = $args[1]
 $kontokirjeldus = $args[2]
 $parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+# Loome kasutaja sisestatud parameetritest lähtuvalt
 New-LocalUser "$kasutajanimi" -Password $parool -FullName "$taisnimi" -Description "$kontokirjeldus"
 }
