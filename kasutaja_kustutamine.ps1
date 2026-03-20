689c12072585cde97868333f8f43a533f7a8c21c
Sander Orav <sander.orav@voco.ee>
2026-03-20 06:50:42 +0000

    Ülesanne 2 - Lokaalsete kasutajate kustutamine süsteemist

--- kasutaja_kustutamine.ps1
+++ kasutaja_kustutamine.ps1
@@ -2,9 +2,24 @@
 $eesnimi = Read-Host "Sisesta eesnimi"
 $perenimi = Read-Host "Sisesta perenimi"
 
-# Teeme väiketähtedeks ja kasutajanimeks
+# Teeme väiketähtedeks
 $eesnimi = $eesnimi.ToLower()
 $perenimi = $perenimi.ToLower()
+
+# Asendame täpitähed
+$eesnimi = $eesnimi `
+    -replace 'õ','o' `
+    -replace 'ä','a' `
+    -replace 'ö','o' `
+    -replace 'ü','u'
+
+$perenimi = $perenimi `
+    -replace 'õ','o' `
+    -replace 'ä','a' `
+    -replace 'ö','o' `
+    -replace 'ü','u'
+
+# Loome kasutajanime
 $kasutajanimi = "$eesnimi.$perenimi"
 
 # Küsime kasutajalt kinnitust
@@ -12,16 +27,18 @@ $jahei = Read-Host "Kas kustutan kasutaja '$kasutajanimi'? Kinnitamiseks sisesta
 $jahei = $jahei.ToLower()
 
 if ($jahei -eq 'jah') {
-try {
-# Kontrollime, kas kasutaja on olemas
-$kasutaja = Get-LocalUser -Name $kasutajanimi -ErrorAction Stop
+    try {
+        # Kontrollime, kas kasutaja on olemas
+        $kasutaja = Get-LocalUser -Name $kasutajanimi -ErrorAction Stop
 
-# Kustutame kasutaja
-Remove-LocalUser -Name $kasutajanimi
-Write-Host "Kasutaja '$kasutajanimi' on edukalt kustutatud." -ForegroundColor Green
-} catch {
-Write-Host "Viga: Kasutajat '$kasutajanimi' ei leitud või pole võimalik seda kustutada." -ForegroundColor Red
+        # Kustutame kasutaja
+        Remove-LocalUser -Name $kasutajanimi
+        Write-Host "Kasutaja '$kasutajanimi' on edukalt kustutatud." -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Viga: Kasutajat '$kasutajanimi' ei leitud või pole võimalik seda kustutada." -ForegroundColor Red
+    }
 }
-} else {
-Write-Host "Kustutamine tühistatud."
+else {
+    Write-Host "Kustutamine tühistatud."
 }
